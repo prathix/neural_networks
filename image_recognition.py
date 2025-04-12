cec5769c1b3f54464cd0ec48a6548c3b42687364
prathix <ritukhanna1717@gmail.com>
2025-04-13 01:01:58 +0200

    starting to fix overfitting problems

--- image_recognition.py
+++ image_recognition.py
@@ -4,24 +4,22 @@ import PIL
 import PIL.Image
 import tensorflow as tf
 import tensorflow_datasets as tfds
+from tensorflow.keras.applications import MobileNetV2
 import pathlib
 import matplotlib.pyplot as plt
 import kagglehub
 
 dataset_url = kagglehub.dataset_download("asdasdasasdas/garbage-classification")
-data_dir = tf.keras.utils.get_file(origin=dataset_url,
-                                   fname='flower_photos',
-                                   untar=True)
-data_dir = pathlib.Path(data_dir)
+data_dir = pathlib.Path(dataset_url)
 
 
-batch_size = 32
+batch_size = 128
 img_height = 64
 img_width = 64
 
 train_ds = tf.keras.utils.image_dataset_from_directory(                             #serve ad esplicitare i dati per l'allenamento e per il testing
     data_dir,
-    validation_split = 0.8,
+    validation_split = 0.2,
     subset = "training",
     seed = 123,
     image_size = (img_height, img_width),
@@ -37,6 +35,9 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   batch_size=batch_size)
 
 class_names = train_ds.class_names
+
+print("Train batches:", len(train_ds))
+print("Val batches:", len(val_ds))
 
 """ plt.figure(figsize=(10, 10))                                                       #serve a fare il grafico delle prime dieci immagini
 for images, labels in train_ds.take(1):
@@ -58,15 +59,24 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 num_classes = 6
 
+data_augmentation = tf.keras.Sequential([
+    tf.keras.layers.RandomFlip("horizontal"),
+    tf.keras.layers.RandomRotation(0.1),
+    tf.keras.layers.RandomZoom(0.1),
+])
+
 model = tf.keras.Sequential([
+  data_augmentation,
   tf.keras.layers.Rescaling(1./255, input_shape=(64, 64, 3)),
-  tf.keras.layers.Conv2D(16, 3, activation='relu'),  # meno filtri
+  tf.keras.layers.Conv2D(16, 3, activation='sigmoid'),  # meno filtri
   tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Conv2D(16, 3, activation='relu'),
+  tf.keras.layers.Conv2D(32, 3, activation='sigmoid'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 3, activation='sigmoid'),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Dense(32, activation='relu'),
+  tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
   tf.keras.layers.Dense(num_classes, activation='softmax')  # aggiungi softmax finale
 ])
 
